8f1a616d627fbf208964252ad11f3c06ad798ba6
Maria <mariaessani493@gmail.com>
2023-08-13 21:59:20 +0500

    commit

---
+++ index.js
@@ -0,0 +1,96 @@
+import inquirer from 'inquirer';
+import chalk from "chalk";
+let account_A = {
+    name: "Ryan",
+    pin: 1235,
+    balance: 40000
+};
+// a sleep function
+const sleep1 = () => {
+    return new Promise((r) => { setTimeout(r, 2000); });
+};
+const sleep2 = () => {
+    return new Promise((r) => { setTimeout(r, 1000); });
+};
+console.log(chalk.bgGray(chalk.red("BANK ATM")));
+await sleep1();
+//to perform basic *ATM TRANSACTIONS*
+async function transaction(tbalance) {
+    const transType = await inquirer.prompt([
+        {
+            message: "Your transaction type is:",
+            type: "list",
+            choices: ["Cash-Withdrawal", "Fast Cash", "Balance Inquiry"],
+            name: "transaction"
+        }
+    ]);
+    console.log("\n");
+    if (transType.transaction == "Cash-Withdrawal") {
+        await sleep2();
+        const amount = await inquirer.prompt([
+            {
+                message: "Type your Amount:",
+                type: "number",
+                name: "userAmount"
+            }
+        ]);
+        if (amount.userAmount > tbalance) {
+            console.log(chalk.yellow(`Your available balance is ${tbalance}`));
+        }
+        else {
+            console.log(`Ok! your cash amount is here: ${chalk.blue(amount.userAmount)}`);
+            tbalance = tbalance - amount.userAmount;
+            console.log(chalk.yellow(`Your available balance is: ${tbalance}`));
+        }
+    }
+    else if (transType.transaction == "Fast Cash") {
+        await sleep2();
+        const amount = await inquirer.prompt([
+            {
+                message: "What amount you want to withdraw:",
+                type: "list",
+                name: "userAmount",
+                choices: ["1000", "5000", "10000", "20000", "25000"]
+            }
+        ]);
+        console.log(`Ok! your cash amount is here: ${chalk.blue(amount.userAmount)}`);
+        tbalance = tbalance - amount.userAmount;
+        console.log(chalk.yellow(`Your available balance is: ${tbalance}`));
+    }
+    else if (transType.transaction == "Balance Inquiry") {
+        await sleep2();
+        console.log(chalk.yellow(`Your available balance is: ${tbalance}`));
+    }
+    return tbalance;
+}
+//function to start the process of transaction recursively
+async function process(proAccount) {
+    do {
+        proAccount.balance = await transaction(proAccount.balance);
+        await sleep1();
+        console.log("\n");
+        var reStart = await inquirer.prompt([
+            {
+                type: "list",
+                name: "moreProcess",
+                message: "Do you want to process more transactions?",
+                choices: ["Yes", "No"]
+            },
+        ]);
+    } while (reStart.moreProcess == "Yes");
+}
+// to take user input, i.e account details
+const answer = await inquirer.prompt([
+    {
+        message: "Enter pin",
+        name: "pin",
+        type: "number"
+    },
+]);
+await sleep2();
+if (answer.pin == account_A.pin) {
+    process(account_A);
+}
+else {
+    console.log("You entered an invalid pin");
+}
---
+++ readme.md
@@ -0,0 +1,36 @@
+Atm
+
+TYPE "npx maria-atm" on command prompt 
+
+
+* YOU CAN WITHDRAW CASH
+* YOU CAN WITHDRAW FAST CASH
+* YOU CAN CHECK BALANCE
+
+This is a dummy atm which can work with the account details as follows:
+      Name: "Ryan",
+      Pin: "1235",
+      Available Balance: "40,000 Rupees"
+
+
+
+first the app will ask to type pin
+If the pin is valid the app will ask the type of transaction 
+*for cashwithdrawal{
+    the app will ask amount 
+    and will subtract that amount of original balance
+}
+*for balance checking{
+    the app will show total amount 
+}   
+*for fast cash{
+    the app will give options to withdraw an amount
+}  
+then the app will ask the option of using more tansactions
+{
+    upon selecting no the process of transaction will be terminated
+}
+
+
+
+
